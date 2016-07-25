c2bb063cf89edc96c427bd98be29348e46f58603
shuiyuelinghui <532665393@qq.com>
2016-07-25 09:50:53 +0800

    update 3files

--- drag.js
+++ drag.js
@@ -2,7 +2,8 @@
 * @Author: Administrator
 * @Date:   2016-07-25 09:25:27
 * @Last Modified by:   Administrator
-* @Last Modified time: 2016-07-25 09:43:07
+* @Last Modified time: 2016-07-25 09:49:52
 */
 
 var b = 2;
+var c = 3;
--- drag2.js
+++ drag2.js
@@ -2,7 +2,8 @@
 * @Author: Administrator
 * @Date:   2016-07-25 09:32:42
 * @Last Modified by:   Administrator
-* @Last Modified time: 2016-07-25 09:32:50
+* @Last Modified time: 2016-07-25 09:50:10
 */
 
 var a =1;
+var b = 2;
